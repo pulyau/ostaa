f35cc077067a9d6c9a713c02469fd6347c5907d7
Pulat <uralovpulya@gmail.com>
2023-10-27 11:20:39 -0700

    Added comments to server code

--- public_html/index.js
+++ public_html/index.js
@@ -18,6 +18,30 @@ function addUser() {
     }
 }
 
+function addItem() {
+    var title = document.getElementById("itemTitle").value;
+    var desc = document.getElementById("descItem").value;
+    var image = document.getElementById("imageItem").value;
+    var price = document.getElementById("priceItem").value;
+    var status = document.getElementById("statusItem").value;
+    var userItem = document.getElementById("forUserItem").value;
+    if (title == "" || info == "" || image == "" || price == "" || status == "" || userItem == "") {
+        window.alert("Please fill out all the information")
+    } else {
+        let info = {title: title, desc: desc, image: image, price: price, status: status, userItem: userItem}
+        let p = fetch('add/item', {
+            method: 'POST',
+            body: JSON.stringify(info),
+            headers: { 'Content-Type': 'application/json'}
+        });
+        p.then((response) => {
+            return response.text();
+        }).catch((err) => {
+            console.log(err);
+        });
+    }
+}
+
 function getUsers() {
     let url = 'http://localhost/get/users';
     fetch(url).then((response) => {
--- server.js
+++ server.js
@@ -5,33 +5,50 @@
  * and it uses express, promises, mongoose to create the server side
  * and DB side using MongoDB
  */
+// Initializing required modules: mongoose, express, body-express,
+// and also initializing the port 80
 const mongoose = require('mongoose');
 const bp = require('body-parser')
 const express = require("express");
 const app = express();
 const port = 80;
 
-//DB stuff
+// Seeting up the databse using mongoose and mongoDB
 const db  = mongoose.connection;
 const mongoDBURL = 'mongodb://127.0.0.1/ostaaItems';
 mongoose.connect(mongoDBURL, { useNewUrlParser: true });
 db.on('error', () => { console.log('MongoDB connection error:') });
 
+// Load public_html using express
 app.use(express.static('public_html'));
 
+// -----------------Schema Section-----------------
 var Schema = mongoose.Schema;
 
+// Creating a new schema for users
 var userSchema = new Schema({
   username: String,
   password: String,
   listings: Array,
   purchases: Array,
 });
-
 var User = mongoose.model('User', userSchema );   
 
+// Creating a new schema for items
+var itemSchema = new Schema({
+  title: String,
+  description: String,
+  image: String,
+  price: Number,
+  stat: String
+});
+var Item = mongoose.model('Item', itemSchema);
+
+// Using json 
 app.use(bp.json());
 
+// POST method. Adds a new user with username and password and adds to the database
+// INCOMPLETE, need to check if the user already exists
 app.post('/add/user', (req, res) => {
   let username = req.body.username;
   let password = req.body.password;
@@ -40,7 +57,13 @@ app.post('/add/user', (req, res) => {
   res.end('Got the name');
 });
 
+// POST method. Adds a new item to the databse
+// INCOMPLETE
+app.post('/add/item', (req, res) => {
+  res.end('Got the name');
+});
 
+// GET method. Returns the json file containing all the users
 app.get('/get/users', function (req, res) {
   let p = User.find({}).exec();
   p.then((documents) => {
@@ -48,4 +71,5 @@ app.get('/get/users', function (req, res) {
   });
 });
 
+// Listening to port 80
 app.listen(port, ()=>{console.log(`Success!!!`)});
